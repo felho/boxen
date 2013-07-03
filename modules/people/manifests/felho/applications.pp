1ca0baef5044abd73636e897ff8e23e7d993e2f7
felho <felho@ustream.tv>
2013-07-03 13:03:47 -0700

    Divvy, Transmission, Dotfiles

--- modules/people/manifests/felho/applications.pp
+++ modules/people/manifests/felho/applications.pp
@@ -33,21 +33,58 @@ class people::felho::applications {
   boxen::osx_defaults {'Clipmenu maxHistorySize':
     domain  => 'com.naotaka.ClipMenu',
     key   => 'maxHistorySize',
-    value => '300',
+    value => 300,
   }  
   boxen::osx_defaults {'Clipmenu numberOfItemsPlaceInline':
     domain  => 'com.naotaka.ClipMenu',
     key   => 'numberOfItemsPlaceInline',
-    value => '20',
+    value => 20,
   }  
   boxen::osx_defaults {'Clipmenu numberOfItemsPlaceInsideFolder':
     domain  => 'com.naotaka.ClipMenu',
     key   => 'numberOfItemsPlaceInsideFolder',
-    value => '20',
+    value => 20,
   }  
   boxen::osx_defaults {'Clipmenu maxMenuItemTitleLength':
     domain  => 'com.naotaka.ClipMenu',
     key   => 'maxMenuItemTitleLength',
-    value => '25',
+    value => 25,
+  }
+
+  # Transmission
+  include transmission
+  file {"/Users/felho/Library/Application Support/Transmission":
+     ensure  => 'link',
+     target  => "/Users/felho/Dropbox/appconfig/Transmission/",
+     force   => true,
+     require => [Package[Dropbox],Package[transmission]],
   }  
+
+  # Dotfiles
+  file {"/Users/felho/.profile":
+     ensure  => 'link',
+     target  => "/Users/felho/Dropbox/config/.profile",
+     force   => true,
+     require => Package[Dropbox],
+  }  
+  file {"/Users/felho/.alias":
+     ensure  => 'link',
+     target  => "/Users/felho/Dropbox/config/.alias",
+     force   => true,
+     require => Package[Dropbox],
+  }  
+  file {"/Users/felho/.bash_history":
+     ensure  => 'link',
+     target  => "/Users/felho/Dropbox/config/.bash_history",
+     force   => true,
+     require => Package[Dropbox],
+  }  
+
+  # Divvy
+  include divvy
+  boxen::osx_defaults {'Divvy useGlobalHotkey':
+    domain  => 'com.mizage.Divvy',
+    key   => 'useGlobalHotkey',
+    value => 1,
+  }
 }
