a08d8b6f4025a0b759bef52066a15bedb7b1e3fb
felho <felho@ustream.tv>
2013-07-07 06:07:19 -0700

    setting first dotfiles

--- modules/people/manifests/felho.pp
+++ modules/people/manifests/felho.pp
@@ -1,8 +1,8 @@
 class people::felho {
+  include people::felho::dotfiles
   include people::felho::osx_settings
   include people::felho::applications
   include people::felho::application_settings
-  include people::felho::dotfiles
   include people::felho::homebrew_packages
 
   notify { 'class people::felho declared': }
