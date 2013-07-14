f8849ae218382c1e45fa550ea2ae630237d5745b
felho <felho@ustream.tv>
2013-07-14 11:30:28 -0700

    transmission app settings

--- modules/people/manifests/felho/application_settings/transmission.pp
+++ modules/people/manifests/felho/application_settings/transmission.pp
@@ -5,4 +5,36 @@ class people::felho::application_settings::transmission {
     force   => true,
     require => [ Package['Dropbox'], Package['transmission'] ],
   }
+
+  boxen::osx_defaults { 'Disable total download rate on badge icon':
+    user   => $::luser,
+    domain => 'org.m0k.transmission',
+    key    => 'BadgeDownloadRate',
+    type   => 'bool',
+    value  => false,
+  }
+
+  boxen::osx_defaults { 'Disable total upload rate on badge icon':
+    user   => $::luser,
+    domain => 'org.m0k.transmission',
+    key    => 'BadgeUploadRate',
+    type   => 'bool',
+    value  => false,
+  }
+
+  boxen::osx_defaults { 'Turn off prompt for removal of active transfers':
+    user   => $::luser,
+    domain => 'org.m0k.transmission',
+    key    => 'CheckQuit',
+    type   => 'bool',
+    value  => false,
+  }
+
+  boxen::osx_defaults { 'Turn off prompt for quiting with active transfers':
+    user   => $::luser,
+    domain => 'org.m0k.transmission',
+    key    => 'CheckRemove',
+    type   => 'bool',
+    value  => false,
+  }
 }
