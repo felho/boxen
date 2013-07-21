0bba433431a94e10b767d8fdb693c4f67c3fecc9
felho <felho@ustream.tv>
2013-07-21 02:18:14 -0700

    adding hungarian keyboard layout

--- modules/people/manifests/felho/osx_settings/keyboard.pp
+++ modules/people/manifests/felho/osx_settings/keyboard.pp
@@ -4,4 +4,5 @@ class people::felho::osx_settings::keyboard {
   include people::felho::osx_settings::keyboard::fn_plus_fkey_for_special
   include people::felho::osx_settings::keyboard::fast_repeat_rate
   include people::felho::osx_settings::keyboard::input_source_per_document
+  include people::felho::osx_settings::keyboard::hungarian_layout
 }
